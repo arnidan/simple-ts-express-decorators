d830d761bc18683c48f18268b7ec4b3dbb2d409c
Andrew <5755685+arnidan@users.noreply.github.com>
2020-11-03 19:13:49 +0300

    Fix async functions handling & migrate to GA (#29)
    * fix async functions errors handling
    
    * v1.1.0-0
    
    * fix deps
    
    * v1.1.0-1
    
    * try another method
    
    * v1.1.0-2
    
    * use router
    
    * revert
    
    * v1.1.0-3
    
    * use router
    
    * v1.1.0-4
    
    * remove router
    
    * v1.1.0-5
    
    * use yarn
    
    * migrate to github actions
    
    * fix versions
    
    * fix coverage
    
    * fix
    
    * fix coverage

--- src/ControllersLoader.ts
+++ src/ControllersLoader.ts
@@ -1,4 +1,4 @@
-import {Express, Request, Response} from 'express';
+import {Express, NextFunction, Request, Response} from 'express';
 import {importClassesFromDirectories} from './utils/importClassesFromDirectories';
 import {RouteDefinition} from './RouteDefinition';
 import * as path from 'path';
@@ -25,9 +25,10 @@ export class ControllersLoader {
       const routes: Array<RouteDefinition> = Reflect.getMetadata('routes', controller);
 
       routes.forEach(route => {
-        app[route.requestMethod](path.join('/', prefix, route.path), (req: Request, res: Response) => {
-          instance[route.methodName](req, res);
-        });
+        app[route.requestMethod](
+          path.join('/', prefix, route.path),
+          (req: Request, res: Response, next: NextFunction) => Promise.resolve(instance[route.methodName](req, res)).catch(next),
+        );
       });
     }
   }
